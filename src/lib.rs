6009104dfcb5880f07c320bf78cf299a99cbc47e
Plucky <plucky@example.com>
2023-11-10 20:35:05 +0800

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /*
  * @Author: anger
  * @Date: 2023-11-10 15:42:55
- * @LastEditTime: 2023-11-10 20:32:01
+ * @LastEditTime: 2023-11-10 20:34:53
  */
 
 
@@ -12,6 +12,7 @@ pub use etcd_discovery::*;
 pub use etcd_register::*;
 
 // #[doc(hidden)]
+/// Re-export etcd_client
 #[doc(inline)]
 pub use etcd_client;
 pub use etcd_client::ConnectOptions;
